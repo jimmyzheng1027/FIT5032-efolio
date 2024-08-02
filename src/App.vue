6290045bda50a7fc7fa27e649c35f28a14f5430d
Jimmy <zhengyj1027@gmail.com>
2024-08-02 16:36:00 +0800

    FIT5032 week1&week2

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import JSON from './components/JSON.vue'
+
+</script>
+
+<template>
+
+  <main>
+    <JSON />
+  </main>
+</template>
+
+
+<style>
+.highlighted-author {
+  font-weight: bold;
+  color: blue;
+}
+</style>
